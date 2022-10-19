95b112998b775fd1f4ae8ce1d4aa38c03972f6e7
George Roth <g@georgeroth.eu>
2022-10-19 15:04:15 +0100

    Adding dog buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 console.log(data);
 
 // WRITE YOUR CODE BELOW!
+
+const mainUl = document.querySelector('.dogs-list')
+
+data.forEach(dog => {
+    const dogButton = document.createElement('li')
+    dogButton.innerText = dog.name
+    dogButton.setAttribute('class', 'dogs-list__button')
+    mainUl.appendChild(dogButton)
+})
